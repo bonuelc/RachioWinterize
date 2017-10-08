e7ee6b9cf9b97b3529c02946a68d5125cf2dbc26
Christopher Bonuel <bonuelc@gmail.com>
2017-10-08 12:44:43 -0600

    Initial Commit

---
+++ Winterize/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Winterize
+//
+//  Created by Christopher Bonuel on 10/8/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
