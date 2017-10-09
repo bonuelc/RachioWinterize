3be9717470d393012befe1d6d083e215e3b4601c
Christopher Bonuel <bonuelc@gmail.com>
2017-10-08 19:56:39 -0600

    Add enum RachioService with a 'person' endpoint
    This will be used to grab the id for the logged-in user.

---
+++ Winterize/RachioService.swift
@@ -0,0 +1,14 @@
+//
+//  RachioService.swift
+//  Winterize
+//
+//  Created by Christopher Bonuel on 10/8/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import Foundation
+
+enum RachioService {
+    case person
+}
+
