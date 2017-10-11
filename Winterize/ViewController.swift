a3b4a8d9f76ef70a7fff18b596e3fd523495033c
Christopher Bonuel <bonuelc@gmail.com>
2017-10-11 14:57:10 -0600

    Add tableView to ViewController

--- Winterize/ViewController.swift
+++ Winterize/ViewController.swift
@@ -9,5 +9,23 @@
 import UIKit
 
 class ViewController: UIViewController {
+    // MARK: - UI
+    
+    lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.translatesAutoresizingMaskIntoConstraints = false
+        return tableView
+    }()
+    
+    override func viewWillLayoutSubviews() {
+        view.addSubview(tableView)
+        
+        NSLayoutConstraint.activate([
+            tableView.topAnchor.constraint(equalTo: view.topAnchor),
+            tableView.leftAnchor.constraint(equalTo: view.leftAnchor),
+            tableView.rightAnchor.constraint(equalTo: view.rightAnchor),
+            tableView.bottomAnchor.constraint(equalTo: view.bottomAnchor)
+        ])
+    }
 }
 
