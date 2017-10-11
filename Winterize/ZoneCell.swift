7a71bd06735216447bc9123e16746b70a1d94c8e
Christopher Bonuel <bonuelc@gmail.com>
2017-10-11 14:59:28 -0600

    Add ZoneCell class

---
+++ Winterize/ZoneCell.swift
@@ -0,0 +1,12 @@
+//
+//  ZoneCell.swift
+//  Winterize
+//
+//  Created by Christopher Bonuel on 10/11/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import UIKit
+
+class ZoneCell: UITableViewCell {
+}
