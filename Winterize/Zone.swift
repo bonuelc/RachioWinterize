667fa1d643b3120cb972f5e162ccec152d7074f5
Christopher Bonuel <bonuelc@gmail.com>
2017-10-11 16:51:42 -0600

    Add class Zone: Object

---
+++ Winterize/Zone.swift
@@ -0,0 +1,15 @@
+//
+//  Zone.swift
+//  Winterize
+//
+//  Created by Christopher Bonuel on 10/10/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+import RealmSwift
+
+class Zone: Object {
+    dynamic var id = ""
+    dynamic var number = 0
+    dynamic var name = ""
+}
