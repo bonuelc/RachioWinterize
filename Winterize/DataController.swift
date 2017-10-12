09adc95bc5a1f9b431cbb11a2c215d4dd9cd7ec2
Christopher Bonuel <bonuelc@gmail.com>
2017-10-12 09:35:45 -0600

    Add DataController class

---
+++ Winterize/DataController.swift
@@ -0,0 +1,11 @@
+//
+//  DataController.swift
+//  Winterize
+//
+//  Created by Christopher Bonuel on 10/12/17.
+//  Copyright © 2017 Christopher Bonuel. All rights reserved.
+//
+
+class DataController {
+    
+}
